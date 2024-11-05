f47b67c6db548a857294f61f5350a116b38f4c86
bufan <1395470761@qq.com>
2024-11-05 21:26:15 +0800

    cascade_model

--- CascadeModel/llms.py
+++ CascadeModel/llms.py
@@ -3,10 +3,10 @@ import sys
 import json
 import os
 
-sys.path.append('/mnt/user/bufan/LALM')
+sys.path.append('/../')
 from openai_tool import GetOpenAI
 
-openai_tool = GetOpenAI()  # 需要改为qwen2-7b
+openai_tool = GetOpenAI()
 
 
 def input_single_audio(transcription):
@@ -27,40 +27,29 @@ def input_single_audio(transcription):
 
 
 def process_transcriptions(directory):
-    # 指定 JSON 文件的路径
     json_path = os.path.join(directory, 'transcription.json')
 
-    # 读取 JSON 文件
     with open(json_path, 'r', encoding='utf-8') as file:
         data = json.load(file)
 
-    # 处理每个字典中的 'transcription' 属性
     for item in data:
         transcription_text = item['transcription']
-        # 调用 fun 函数处理 transcription_text，得到 llm_response
         llm_response = input_single_audio(transcription_text)
-        # 向字典中添加 'llm_response' 属性
         item['llm_response'] = llm_response
 
-    # 指定新 JSON 文件的存储路径
     new_json_path = os.path.join(directory, 'llm_response.json')
 
-    # 将更新后的数据存储到新的 JSON 文件
     with open(new_json_path, 'w', encoding='utf-8') as file:
         json.dump(data, file, ensure_ascii=False, indent=4)
 
 def list_subdirectories(base_path):
-    # 获取base_path下的所有项
     entries = os.listdir(base_path)
-
-    # 过滤出目录项
     return [os.path.join(base_path, entry) for entry in entries if os.path.isdir(os.path.join(base_path, entry))]
 
 
 
 if __name__ == '__main__':
-    base_path = '/mnt/user/bufan/lzy/prompt4_10.23/input'
+    base_path = 'PathToYourDataset/input'
 
     for dir_path in list_subdirectories(base_path):
         process_transcriptions(dir_path)
-    pass
--- CascadeModel/whisper_asr.py
+++ CascadeModel/whisper_asr.py
@@ -7,7 +7,7 @@ import json
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
 
-model_id = "/mnt/user/bufan/model_cache/whisper-large-v3"
+model_id = "openai/whisper-large-v3"
 
 model = AutoModelForSpeechSeq2Seq.from_pretrained(
     model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
@@ -27,20 +27,15 @@ pipe = pipeline(
 
 
 def process_directory(directory):
-    # 存储结果的列表
     results = []
 
-    # 遍历目录
     for root, dirs, files in os.walk(directory):
         for file in files:
             if file.endswith(('.wav', '.mp3')):
-                # 构造完整的文件路径
                 file_path = os.path.join(root, file)
 
-                # 调用处理函数
                 transcription = pipe(file_path)["text"]
 
-                # 保存文件名和转录结果到字典中
                 results.append({
                     'filename': os.path.relpath(file_path, start=directory),
                     'transcription': transcription
@@ -52,17 +47,13 @@ def process_directory(directory):
 
 
 def list_subdirectories(base_path):
-    # 获取base_path下的所有项
     entries = os.listdir(base_path)
-
-    # 过滤出目录项
     return [os.path.join(base_path, entry) for entry in entries if os.path.isdir(os.path.join(base_path, entry))]
 
 
 
 if __name__ == '__main__':
-    # 指定要检查的根目录
-    base_path = '/mnt/user/bufan/lzy/prompt4_10.23/input'
+    base_path = 'PathToYourDataset/input'
 
     for dir_path in list_subdirectories(base_path):
         process_directory(dir_path)
